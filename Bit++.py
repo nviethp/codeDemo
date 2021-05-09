9949d6d2aab8e77c67d3f1a86b39f7cf7a707c9f
CodeFore <nviethp@gmail.com>
2021-05-09 21:29:06 +0700

    Creat a new file

---
+++ Bit++.py
@@ -0,0 +1,9 @@
+n = int(input())
+x = 0
+for i in range(n):
+    bit_operation = input()
+    if bit_operation == "X++" or bit_operation == "++X":
+        x = x + 1
+    elif bit_operation == "X--" or bit_operation == "--X":
+        x = x - 1
+print(x)
